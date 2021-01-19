b0467bb9a52efe2c8647b99663c2b3a95e818278
ParikPanchal <parik.panchal@bcgdv.com>
2021-01-19 12:07:49 +0000

    feat: added necessary vars

---
+++ _vars.tf
@@ -0,0 +1,71 @@
+# REQUIRED - CUSTOMIZABLE
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "specifies the region for deployment"
+}
+
+# REQUIRED - CUSTOMIZABLE
+variable "account_id" {
+  type        = string
+  default     = "212285161954"
+  description = "specifies the unique AWS account number for deployment"
+}
+
+# REQUIRED - CUSTOMIZABLE
+variable "cluster_name" {
+  default     = "bcg-dv-microenterprise-stack"
+  description = "specifies the unique eks cluster name"
+}
+
+# OPTIONAL - EXTENDABLE
+variable "map_accounts" {
+  description = "Specifies additional AWS account numbers to add to the aws-auth configmap"
+  type        = list(string)
+
+  default = [
+    "111111111111",
+    "222222222222",
+  ]
+
+}
+# OPTIONAL - EXTENDABLE
+variable "map_roles" {
+  description = "Specifies additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+}
+
+# OPTIONAL - EXTENDABLE
+variable "map_users" {
+  description = "Specifies additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+}
