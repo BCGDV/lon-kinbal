afda02062a2802e777ac1bad1fa29e69c9313af2
ParikPanchal <parik.panchal@bcgdv.com>
2021-01-20 11:33:14 +0000

    fix: fixed fargate profile namespace name

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -25,11 +25,11 @@ resource "aws_eks_fargate_profile" "aws_eks_fargate_profile1" {
   depends_on = [module.eks]
 
   cluster_name           = var.cluster_name
-  fargate_profile_name   = "fg_profile_default_namespace"
+  fargate_profile_name   = "fg_profile_microservices_namespace"
   pod_execution_role_arn = aws_iam_role.iam_role_fargate.arn
   subnet_ids             = module.vpc.aws_subnet_private_prod_ids
   selector {
-    namespace = "default"
+    namespace = "microservices"
   }
 }
 
