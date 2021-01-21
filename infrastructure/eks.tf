e255e25e1aa936084b4d9048d0edb04938d343a2
ParikPanchal <parik.panchal@bcgdv.com>
2021-01-21 22:08:02 +0000

    wip

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -1,10 +1,19 @@
+# Expose important cluster attributes
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
 # Creates the managed control plane
 module "eks" {
   source          = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v12.2.0"
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = module.vpc.aws_vpc_id
-  subnets         = module.vpc.aws_subnet_private_prod_ids
+  subnets         = module.vpc.private_subnets
 
   # The Node groups - for static EC2 instances
   node_groups = {
@@ -28,7 +37,7 @@ resource "aws_eks_fargate_profile" "aws_eks_fargate_profile1" {
   cluster_name           = var.cluster_name
   fargate_profile_name   = "fg_profile_microservices_namespace"
   pod_execution_role_arn = aws_iam_role.iam_role_fargate.arn
-  subnet_ids             = module.vpc.aws_subnet_private_prod_ids
+  subnet_ids             = module.vpc.private_subnets_ids
   selector {
     namespace = "microservices"
   }
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,12 +1,24 @@
 # Creates a VPC for the cluster with an opinionated network architecture
 module "vpc" {
-  source = "git::ssh://git@github.com/reactiveops/terraform-vpc.git?ref=v5.0.1"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.47.0"
 
-  aws_region = "us-east-1"
-  az_count   = 3
-  aws_azs    = "us-east-1a, us-east-1b, us-east-1c"
+  name                 = "${var.cluster_name}-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  global_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
