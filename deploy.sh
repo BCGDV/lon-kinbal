b5d553055e6a8898b4d2f67d59fe16a9f9a5f936
ParikPanchal <parik.panchal@bcgdv.com>
2021-01-26 17:52:41 +0000

    refactor: changed deploy script and location of admin service account policy

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,11 @@ kubectl get deployment metrics-server -n kube-system
 sleep 5s
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/aio/deploy/recommended.yaml
 sleep 5s
-kubectl apply -f ./eks-admin-service-account.yaml
+kubectl apply -f ./support/eks-admin-service-account.yaml
 sleep 5s
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
 kubectl proxy
 sleep 5s
-open 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
+open -a "Google Chrome" 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
 
 kubectl describe ingress -A
