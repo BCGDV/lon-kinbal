92d15455529dd1c69db7dcfd44e5fd114338ca59
ParikPanchal <parik.panchal@bcgdv.com>
2021-01-26 14:12:42 +0000

    refactor: changed setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,16 @@
 cd ./infrastructure
 terraform init
 terraform apply
-aws eks update-kubeconfig --name microenterprise
+aws eks update-kubeconfig --name microenterprise-dev
+
+# Deploy microservices
+cd ..
+cd microservices/service-1
+kubectl apply -f ./deployment.yaml
+cd ../service-2
+kubectl apply -f ./deployment.yaml
+cd ../service-3
+kubectl apply -f ./deployment.yaml
 
 # Deploy dashboard
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
@@ -17,14 +26,5 @@ kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | gre
 kubectl proxy
 sleep 5s
 open 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
-
-# Deploy microservices
-cd ..
-cd microservices/service-1
-kubectl apply -f ./deployment.yaml
-cd ../service-2
-kubectl apply -f ./deployment.yaml
-cd ../service-3
-kubectl apply -f ./deployment.yaml
 
 kubectl describe ingress -A
