b84fa0989a9bdfbc94b35aa4941ebea711cbf4af
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-02 19:13:57 +0000

    feat: added db connection and example endpoints

--- microservices/service-1/index.js
+++ microservices/service-1/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const AWS = require("aws-sdk")
 const eventbridge = new AWS.EventBridge()
@@ -6,18 +7,16 @@ const eventbridge = new AWS.EventBridge()
 const app = express()
 const port = 8080
 
-const {pool} = require("./db")
+const { pool } = require("./db")
+
+app.use(bodyParser.json())
 
 app.get('/', async (req, res) => {
     try {
         console.log(`${req.url} ${req.method} ${Math.round((new Date()).getTime() / 1000)}`)
-        const dbRes = await pool.query('SELECT * from users ORDER BY random() LIMIT 3;');
-        const users = dbRes.rows;
-        console.log("fetched users => ", users)
         res.status(200).send({
             service: 'service-1',
-            res: `Request received on ${Math.round((new Date()).getTime() / 1000)}`,
-            dbRes: users
+            res: `Request received on ${Math.round((new Date()).getTime() / 1000)}`
         })
     } catch (e) {
         console.log(e)
@@ -30,6 +29,31 @@ app.get('/ping', (req, res) => {
         service: 'service-1',
         res: 'PONG'
     })
+})
+
+app.post('/users/create', async (req, res) => {
+    try {
+        console.log(`${req.url} ${req.method} ${Math.round((new Date()).getTime() / 1000)}`)
+        const { name } = req.body
+        const dbRes = await pool.query(`INSERT INTO users(name, createdat) VALUES ('${name}', '${new Date().toUTCString()}') RETURNING *`)
+        // emit user created event
+        const user = dbRes.rows;
+        res.status(200).send({ res: user })
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+app.get('/users/get', async (req, res) => {
+    try {
+        console.log(`${req.url} ${req.method} ${Math.round((new Date()).getTime() / 1000)}`)
+        const dbRes = await pool.query('SELECT * from users ORDER BY random() LIMIT 3;');
+        const users = dbRes.rows;
+        console.log("fetched users => ", users)
+        res.status(200).send({ res: users })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 app.listen(port, () => {
