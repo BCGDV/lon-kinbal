8aa0e6e57702ca9574f02fbd8e2091270b62fc35
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-03 14:52:19 +0000

    rollback: removed alb-ingress-controller

--- infrastructure/modules/eks/eks_stack.tf
+++ infrastructure/modules/eks/eks_stack.tf
@@ -15,14 +15,6 @@ data "aws_eks_cluster_auth" "cluster" {
 data "aws_availability_zones" "available" {
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
-
 # Creates the managed control plane
 module "eks" {
   source          = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v12.2.0"
@@ -226,35 +218,6 @@ resource "aws_iam_policy" "worker_policy" {
     ]
 }
   EOF
-}
-
-provider "helm" {
-  version = "1.3.1"
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
-  }
-}
-
-resource "helm_release" "ingress" {
-  name       = "ingress"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  values = [<<EOF
-  clusterName: ${var.cluster_name}
-  region: ${var.region}
-  namespace: "microservices"
-  vpcId: ${module.vpc.vpc_id}
-  hostNetwork: true
-  EOF
-  ]
-
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
 }
 
 # Creates a VPC
