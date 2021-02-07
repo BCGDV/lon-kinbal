c634b1fd2a7c8fed8155f7f3b20651aa5fba724d
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-07 19:00:26 +0000

    fix: fixed and update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,47 @@
 # Deploy infrastructure
 cd ./infrastructure
 terraform init
-terraform apply
+terraform apply -auto-approve
 aws eks update-kubeconfig --name microenterprise-dev
 
-# Deploy microservices
-cd ../microservices
+# Attach policies to kub2iam-ingress-role - used by API Gateway Ingress Controller to create AWS resources
+aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AutoScalingFullAccess  
+aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator
+aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess 
+aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess 
+aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess
+
+# Configure helm access with RBAC
+cd ../support
+kubectl apply -f helm-rbac.yaml
+helm init --service-account tiller
+sleep 5
+
+# Deploy kube2iam chart
+helm install --set rbac.create=true --set host.iptables=true --set host.interface=eni+ --set=extraArgs.auto-discover-base-arn= stable/kube2iam
+sleep 5
+
+# Deploy API Gateway Ingress Controller Chart
+cd ./AmazonAPIGWHelmChart
+helm install --debug ./amazon-apigateway-ingress-controller --set image.repository="karthikk296d/aws-apigw-ingress-controller"
+sleep 5
+
+# Deploy Fargate namespace
 kubectl apply -f ./fargate-namespace.yaml
-cd service-1
-kubectl apply -f ./deployment.yaml && aws rds create-db-instance --db-name service1db --db-instance-identifier microenterprise-service-1 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
-cd ../service-2
-kubectl apply -f ./deployment.yaml && aws rds create-db-instance --db-name service2db --db-instance-identifier microenterprise-service-2 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
-cd ../service-3
-kubectl apply -f ./deployment.yaml && aws rds create-db-instance --db-name service3db --db-instance-identifier microenterprise-service-3 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
+
+# Deploy microservices
+cd ../../microservices
+cd service-1/kubernetes-config/
+kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
+cd ../service-2/kubernetes-config/
+kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
+cd ../service-3/kubernetes-config/
+kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
+
+# Create persistence layer
+aws rds create-db-instance --db-name service1db --db-instance-identifier microenterprise-service-1 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
+aws rds create-db-instance --db-name service2db --db-instance-identifier microenterprise-service-2 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
+aws rds create-db-instance --db-name service3db --db-instance-identifier microenterprise-service-3 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
 
 # Deploy dashboard
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
@@ -21,6 +50,6 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/a
 kubectl apply -f ./support/eks-admin-service-account.yaml
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
 kubectl proxy
-open -a "Google Chrome" 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
+tput setaf 4; echo "Please open" 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
 
 kubectl describe ingress -A
