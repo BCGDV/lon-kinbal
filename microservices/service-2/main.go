262649546f3cb06251687896e21c963c5ad42263
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-08 10:58:06 +0000

    refactor: standardised services

--- microservices/service-2/main.go
+++ microservices/service-2/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+// Message ...
+type Message struct {
+	Service string
+	Res     string
+}
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "PONG")
+	})
+
+	http.HandleFunc("/service/info", func(w http.ResponseWriter, r *http.Request) {
+		timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		timestampRes := fmt.Sprintf("Request received on %s", timestamp)
+		m := &Message{"service-2", timestampRes}
+		b, err := json.Marshal(m)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Fprintf(w, string(b))
 	})
 
 	fmt.Printf("server running on port %d\n", 8081)
