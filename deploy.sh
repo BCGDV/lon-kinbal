f6553b0cd151349726b3806a46b03a8a63a3a67e
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-08 14:49:42 +0000

    refactor: cleaned deploy script and modified docs

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,44 @@
 # Deploy infrastructure
 cd ./infrastructure
+tput setaf 4; echo "Provisioning EKS cluster"
 terraform init
 terraform apply -auto-approve
 aws eks update-kubeconfig --name microenterprise-dev
 
-# Attach policies to kub2iam-ingress-role - used by API Gateway Ingress Controller to create AWS resources
-aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AutoScalingFullAccess  
-aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator
-aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess 
-aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess 
-aws iam attach-role-policy --role-name kube2iam-ingress-role --policy-arn arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess
-
 # Configure helm access with RBAC
+tput setaf 4; echo "Configuring Helm"
 cd ../support
 kubectl apply -f helm-rbac.yaml
+sleep 5
 helm init --service-account tiller
 sleep 5
 
 # Deploy kube2iam chart
+tput setaf 4; echo "Deploying kube2iam chart"
 helm install --set rbac.create=true --set host.iptables=true --set host.interface=eni+ --set=extraArgs.auto-discover-base-arn= stable/kube2iam
 sleep 5
 
 # Deploy API Gateway Ingress Controller Chart
 cd ./AmazonAPIGWHelmChart
+tput setaf 4; echo "Deploying API Gateway Ingress Controller Chart"
 helm install --debug ./amazon-apigateway-ingress-controller --set image.repository="karthikk296d/aws-apigw-ingress-controller"
 sleep 5
 
 # Deploy Fargate namespace
-kubectl apply -f ./fargate-namespace.yaml
+tput setaf 4; echo "Deploying Fargate namespace"
+kubectl apply -f ../fargate-namespace.yaml
+sleep 5
 
 # Deploy microservices
 cd ../../microservices
+tput setaf 4; echo "Deploying Microservices"
 cd service-1/kubernetes-config/
 kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
+sleep 5
 cd ../service-2/kubernetes-config/
+sleep 5
 kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
+sleep 5
 cd ../service-3/kubernetes-config/
 kubectl apply -f ./deployment.yaml && kubectl apply -f ./service.yaml && kubectl apply -f ./api_ingress.yaml
 
@@ -43,13 +47,13 @@ aws rds create-db-instance --db-name service1db --db-instance-identifier microen
 aws rds create-db-instance --db-name service2db --db-instance-identifier microenterprise-service-2 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
 aws rds create-db-instance --db-name service3db --db-instance-identifier microenterprise-service-3 --db-instance-class db.t2.micro --engine postgres --allocated-storage 20 --storage-type gp2 --master-username microenterpriseadmin --master-user-password microenterpriseadminpassword --publicly-accessible
 
-# Deploy dashboard
+Deploy dashboard
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
 kubectl get deployment metrics-server -n kube-system
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/aio/deploy/recommended.yaml
 kubectl apply -f ./support/eks-admin-service-account.yaml
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
-kubectl proxy
 tput setaf 4; echo "Please open" 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login'
+kubectl proxy
 
 kubectl describe ingress -A
--- readme.md
+++ readme.md
@@ -89,7 +89,7 @@ Response 200:
 
 - **/ping (GET)**
 
-Functionality: Health check endpoint + demonstrates events layer integration by firing off event into event-bus.
+Functionality: Health check endpoint
 
 Parameters: N/A.
 
@@ -118,7 +118,7 @@ Response 200:
 
 - **/orders/create (POST)**
 
-Functionality: Demonstrates persistence layer functionality by connecting to the microservice database and creating a new order.
+Functionality: Demonstrates persistence functionality by connecting to the microservice database and creating a new order. Demonstrates events layer functionality by firing off order created event into event-bus.
 
 Parameters: N/A.
 
@@ -217,6 +217,14 @@ Response 200:
 
 ## Architecture
 ![Kinbal%20973f0a3ede1d4f78918c0c478b33951d/Kinbal_(1).png](https://d2908q01vomqb2.cloudfront.net/fe2ef495a1152561572949784c16bf23abb28057/2020/02/20/api_ingress_controller_overview.png "Architecture")
+
+## Explanation
+- The API Gateway Ingress Controller watches for Ingress events from the API server. When it finds Ingress resources that satisfy its requirements, it begins the creation of AWS resources.
+- API Gateway API is created, and the specified API Gateway stages outlined in Ingress annotations are created.
+- A Private Network Load Balancer is created for the Ingress resource, and Listeners are created for every port specified in paths configuration.
+- For the API Gateway to communicate to the Private Network Load Balancer, a Private API Gateway VPC Link is created.
+- A TargetGroup is created for the reverse proxies specified in the Ingress resource.
+- The API Gateway Ingress Controller deploys NGINX as the reverse proxy, and the reverse proxy Rules are created for each path specified in the Ingress resource. This ensures that traffic to a specific path is routed to the correct pod.
 
 ## Useful Commands
 
