1f152bca8dc695d6db5d6150cf9ef702b561c0ba
ParikPanchal <parik.panchal@bcgdv.com>
2021-02-16 12:10:52 +0000

    fix: fixed EKS deploy script

--- bin/core/EKS/deploy.sh
+++ bin/core/EKS/deploy.sh
@@ -1,5 +1,5 @@
 # Deploy infrastructure
 cd $(cd $(dirname "$1");pwd)/$(basename "$1")/src/infrastructure
 echo "Provisioning EKS cluster"
-echo terraform init
-echo terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
